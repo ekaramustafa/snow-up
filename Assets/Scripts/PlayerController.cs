cbd5075e47a540d9b37cbba4994ed7f4edddacb4
ebubekir <ekaramustafa20@ku.edu.tr>
2023-02-16 01:57:12 +0300

    basic rotation physics added

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D rigidbody;
+    [SerializeField] private float torqueAmount = 1f;
+    // Start is called before the first frame update
+    void Start()
+    {
+         rigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        handleRotation();
+    }
+
+    void handleRotation()
+    {
+        if (Input.GetKey(KeyCode.LeftArrow))
+        {
+            //rotate to left
+            rigidbody.AddTorque(torqueAmount);
+            
+        }
+        else if(Input.GetKey(KeyCode.RightArrow))
+        {
+            //rotate to right
+            rigidbody.AddTorque(-torqueAmount);
+        }
+        
+
+    }
+}
